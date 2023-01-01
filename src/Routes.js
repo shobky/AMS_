9039f4f5a8f9e63e3bce40316a0fea69c48e7296
shobkeyy <shobkyy@gmail.com>
2023-01-01 03:59:40 +0200

    'lazy_loading'

--- src/Routes.js
+++ src/Routes.js
@@ -1,34 +1,36 @@
 import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router'
-import Auth from './auth/Auth'
+// import Auth from './auth/Auth'
 import Loading from './components/loading/Loading'
-import Pipes from './pages/knowMore/pipes/Pipes'
-import Salvage from './pages/knowMore/Salvage'
 const Home = React.lazy(() => import('./pages/home/Home'))
+const Salvage = React.lazy(() => import('./pages/knowMore/Salvage'))
+const Pipes = React.lazy(() => import('./pages/knowMore/pipes/Pipes'))
+
 
 const Routeing = () => {
     return (
         <Routes>
             <Route path='/' element={
-                <Suspense fallback={<Loading/>}>
-                    <Home/>
+                <Suspense fallback={<Loading />}>
+                    <Home />
                 </Suspense>
             } />
-              <Route path='/auth' element={
-                <Suspense fallback={<Loading/>}>
-                    <Auth/>
+            <Route path='/salvage' element={
+                <Suspense fallback={<Loading />}>
+                    <Salvage />
                 </Suspense>
             } />
-             <Route path='/salvage' element={
-                <Suspense fallback={<Loading/>}>
-                    <Salvage/>
+            <Route path='/tanks&pipes' element={
+                <Suspense fallback={<Loading />}>
+                    <Pipes />
                 </Suspense>
             } />
-              <Route path='/tanks&pipes' element={
-                <Suspense fallback={<Loading/>}>
-                    <Pipes/>
+            {/* <Route path='/auth' element={
+                <Suspense fallback={<Loading />}>
+                    <Auth />
                 </Suspense>
-            } />
+            } /> */}
+
 
         </Routes>
     )
